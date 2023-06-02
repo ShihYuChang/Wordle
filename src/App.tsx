678a5e4df4a5299ef28664a667f8e5b2aad55fe7
Sam Chang <sam21323@gmail.com>
2023-06-02 13:48:58 +0800

    Adding the wrapper layout and initial keyboard event

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useReducer } from 'react';
+import styled from 'styled-components';
 
-function App() {
-  const [count, setCount] = useState(0)
+const answer = 'cake';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Words {
+  word: string;
+  status: 'correct' | 'wrong-place' | 'incorrect';
+  hasSubmit: boolean;
 }
 
-export default App
+function reducer(state: Words[], action: Action) {
+  // console.log(state[0].word);
+  const newWords: Words[] = [...JSON.parse(JSON.stringify(state))];
+  const targetIndex: number = newWords.findIndex((word) => word.word === '');
+  switch (action.type) {
+    case 'PRESS_ENTER':
+      console.log(action.payload);
+      return state;
+    case 'TYPE':
+      newWords[targetIndex === -1 ? 0 : targetIndex].word = action.payload;
+      state = newWords;
+      return state;
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const words: Words[] = new Array(16).fill({
+    word: '',
+    status: '',
+    hasSubmit: false,
+  });
+  const [state, dispatch] = useReducer(reducer, words);
+
+  useEffect(() => {
+    function handleKeyDown(e: KeyboardEvent) {
+      switch (e.key) {
+        case 'Enter':
+          dispatch({ type: 'PRESS_ENTER', payload: e.key });
+          break;
+        default:
+          dispatch({ type: 'TYPE', payload: e.key });
+          break;
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
+  return (
+    <Wrapper>
+      {state.map((word: Words, index: number) => (
+        <Word key={index}>{word.word}</Word>
+      ))}
+    </Wrapper>
+  );
+}
+
+interface Action {
+  type: string;
+  payload: string;
+}
+
+const Wrapper = styled.div`
+  display: grid;
+  height: 500px;
+  width: 500px;
+  grid-template-columns: repeat(4, 1fr);
+  margin: 100px auto;
+`;
+
+const Word = styled.div`
+  border: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 35px;
+`;
+
+export default App;
